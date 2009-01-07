2415cf3101f5ef3f92adddccfa37ed5d2bd3e079
Keeto Obcena <markeeto@gmail.com>
2009-01-08 03:31:14 +0800

    Added the source files

---
+++ Source/Validator/Validator.Extensions.js
@@ -0,0 +1,115 @@
+/*
+
+Script: Validator.Extensions.js
+    Add validation methods to String and Elements
+    
+Author:
+    Mark Obcena <markeeto@gmail.com>
+
+Documentation:
+    See /Docs/Validator.Extensions.md
+    
+License:
+
+    Copyright (c) 2008 Mark Obcena
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+    
+*/
+
+if ($type(Validator) == "hash") {
+    
+    /*
+     Rewrites the Validator:addType method to
+     automatically add magic "isXType" methods.
+    */
+    Validator.addType = function (key, value) {
+        if ($type(key) == "string" && $type(value) == "regexp") {
+            var name = "is" + key.capitalize();
+            this.exps.set(key, value);
+            this.set(name, function (value) { return this.test(value, key); } );
+            var method = {};
+            method[name] = function () { return this.validate(key); };
+            Native.implement([String, Element], method);
+            return true;
+        } else {
+            return false;
+        }
+    };
+    
+    
+    // Adds new methods to String
+    
+    String.implement({
+    
+        validate: function (type) {
+            return Validator.test(this, type);
+        },
+    
+        isEmpty: function () {
+            return Validator.isEmpty(this);
+        },
+    
+        ofLength: function (min, max) {
+            return Validator.ofLength(this, min, max);
+        }
+    
+    });
+    
+    
+    // Adds new methods to Element
+    
+    Element.implement({
+
+        stringable: ["input", "textarea"],
+    
+        validate: function (type) {
+            return this.can_validate() ? Validator.validate(this.value, type) : null;
+        },
+    
+        can_validate: function () {
+          return this.stringable.contains( this.getTag() );
+        },
+    
+        isEmpty: function () {
+            return this.can_validate() ? Validator.isEmpty(this.value) : null;
+        },
+    
+        ofLength: function (min, max) {
+            return this.can_validate() ? Validator.ofLength(this.value, min, max) : null;
+        }
+    
+    });
+    
+    
+    // Add magic "isXType" methods to both String and Element
+    
+    (function() {
+        var methods = {};
+        Validator.exps.getKeys().each(function (key) {
+                var name = "is" + key.capitalize();
+                methods[name] = function () { return this.validate(key); };
+        })
+        Native.implement([String, Element], methods)
+    })();
+
+}
---
+++ Source/Validator/Validator.js
@@ -0,0 +1,98 @@
+/*
+
+Script: Validator.js
+    Basic data validation Library
+    
+Author:
+    Mark Obcena <markeeto@gmail.com>
+    
+Usage:
+    Validator.test(str, type);
+
+Documentation:
+    See /Docs/Validator.md
+    
+License:
+
+    Copyright (c) 2008 Mark Obcena
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+    
+*/
+
+var Validator = new Hash({
+    
+    exps: new Hash({
+        "alpha": /^[a-zA-z\s\D]+$/,
+        "alphaStrict": /^[a-zA-z]+$/,
+        "alphaNum": /^[0-9a-zA-Z\s\D]+$/,
+        "alphaNumStrict": /^[0-9a-zA-Z]+$/,
+		"number": /^[0-9]+$/,
+		"email": /^[a-z0-9_+.-]+\@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/,
+        "URL": /https?:\/\/([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+([a-zA-Z]{2,9})(:\d{1,4})?([-\w\/#~:.?+=&%@~]*)/
+    }),
+    
+    test: function (value, type) {
+        return (this.exps.get(type) || this.exps.get("alphaNum")).test(value);
+    },
+    
+    isEmpty: function (value) {
+        return value.replace(/\s/g, "").length == 0;
+    },
+    
+    ofLength: function (value, min, max) {
+        min = min || 0; 
+        max = max || 10000000000000000;
+        return value.length >= min && value.length <= max;
+    },
+    
+    addType: function (key, value) {
+        if ($type(key) == "string" && $type(value) == "regexp") {
+            var name = "is" + key.capitalize();
+            this.exps.set(key, value);
+            this.set(name, function (value) { return this.test(value, key); } );
+            return true;
+        } else {
+            return false;
+        }
+    },
+    
+    addTypes: function (pairs) {
+        var that = this;
+        if ($type(pairs) == "object") {
+            pairs = $H(pairs);
+            pairs.each(function(value, key) {
+               that.addType(key, value);
+            });
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+});
+
+(function() {
+    Validator.exps.each(function (value, key) {
+        Validator.addType(key, value);
+    });
+})();
